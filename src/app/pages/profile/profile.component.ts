c7ba934ce7b9905f6bd40a9d03a70064f850c455
Luis Peraza <warriorpropvp@hotmail.com>
2024-06-05 17:20:31 -0700

    Rediseño profile se agregó nuevos campos de proyectos

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -9,12 +9,11 @@ import { HttpClient } from '@angular/common/http';
 export class ProfileComponent implements OnInit {
   visible: boolean = false;
   email: string = '';
-  propuesta: string = '';
+  projectProposal: string = '';
 
   constructor(private http: HttpClient) { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
 
   showDialog() {
     this.visible = true;
@@ -23,13 +22,13 @@ export class ProfileComponent implements OnInit {
   enviarDatos() {
     const data = {
       email: this.email,
-      proposal: this.propuesta
+      projectProposal: this.projectProposal // Asegúrate de usar el nombre correcto aquí
     };
 
     this.http.post('http://localhost:8080/contacts', data).subscribe(
       (response) => {
         console.log('Datos enviados con éxito:', response);
-        this.visible = false; // Close the dialog
+        this.visible = false; // Cerrar el diálogo
 
       },
       (error) => {
